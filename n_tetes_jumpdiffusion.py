07c69fcdec4acc9728ae3076cbd1ff0d872cd52f
DamienLM <lemoel.damien@gmail.com>
2026-06-18 15:00:12 +0200

    Modif sur s + modif sur K10

--- n_tetes_jumpdiffusion.py
+++ n_tetes_jumpdiffusion.py
@@ -4,7 +4,7 @@ import random
 
 
 
-N = 1 #number of heads we are going to simulate
+N = 15 #number of heads we are going to simulate
 delta = [0 for i in range(N)] #random position shifts for the heads
 v = 2 #viscosity of the sarcomere's surroundings
 F = 0 #force exerted on the actin filament
@@ -49,7 +49,7 @@ b = 1/kBTemp #inverse temperature
 ny = 10.288 #inverse viscosity coefficients (fluidity) (ms-1.pN-1.nm)
 nx = 10.288
 
-T = 1500  #max time of the simulation, in ms
+T = 400  #max time of the simulation, in ms
 dt = 0.01    #time step (ms)
 nsteps = int(T/dt)    #number of steps in the simulation
 
@@ -71,7 +71,7 @@ def heaviside(s) :
     return 1
 
 K01 = lambda x, y, s : k01*heaviside(l0 - y)*heaviside(l/2 - abs(x - s)) #0.1 + kmax*(1 - np.tanh(alphay*(y - l0)))*(0.5*(1 - heaviside(s))*(1 + np.tanh(alphas*(s + sl01))) + 0.5*heaviside(s)*(1 - np.tanh(alphas*(s - sr01))))   #direct transition rates
-K10 = lambda x, y, s : k10*heaviside(l/2 - abs(x - s))*(heaviside(y - l0) + 10*heaviside(3 - s))
+K10 = lambda x, y, s : k10*heaviside(l/2 - abs(x - s))*(heaviside(y - l0))+(1/(d/2-x))**2#
 #note that K10 is supposed to vanish outside of [-l/2, l/2]
 
 E = 80 #energy shift in zJ
@@ -88,12 +88,14 @@ dyw1 = lambda x, y : k*(x + y) + d_double_well(y + sbar1, k1pre, k1post, l1, y1p
 
 muT = 100   #shift due to ATP consumption, in zJ
 
+# plot energy profil
 
-plt.plot([dx*t - d/4 for t in range(4*npos)], [w0(0, dx*t - d/4) for t in range(4*npos)], label = 'detached')
-plt.plot([dx*t - d/4 for t in range(4*npos)], [w1(0, dx*t - d/4) for t in range(4*npos)], label = 'attached')
-plt.plot([dx*t - d/4 for t in range(4*npos)], [w0(0, dx*t - d/4) - muT for t in range(4*npos)], label = 'detached')
+# plt.plot([dx*t - d/4 for t in range(4*npos)], [w0(0, dx*t - d/4) for t in range(4*npos)], label = 'detached')
+# plt.plot([dx*t - d/4 for t in range(4*npos)], [w1(0, dx*t - d/4) for t in range(4*npos)], label = 'attached')
+# plt.plot([dx*t - d/4 for t in range(4*npos)], [w0(0, dx*t - d/4) - muT for t in range(4*npos)], label = 'detached')
+# plt.legend()
 
-plt.show()
+# plt.show()
 
 
 
@@ -107,17 +109,12 @@ K10rev = lambda x, y, s : h*K10(x, y, s)*np.exp(b*((w0(x, y) - muT) - w1(s, y)))
 
 
 alpha = np.array([[0 for t in range(nsteps)] for i in range(N)])      #actual description of the stochastic process
-s = np.array([5 for t in range(nsteps)], dtype = float)
-X = np.array([[s[0] + delta[i] for t in range(nsteps)] for i in range(N)], dtype = float)
+s0 = d * (np.random.random(size=N) - 0.5) #Initial Condition
+s = np.array([[s0[i] for t in range(nsteps)] for i in range(N)], dtype = float)
+X = np.array([[s[i,0] for t in range(nsteps)] for i in range(N)], dtype = float)
 Y = np.array([[0 for t in range(nsteps)] for i in range(N)], dtype = float)
 
 for t in range(nsteps - 1):
-  #Compute the rates K and K_rev
-  K01t= K01(X[i, t], Y[i, t], s[t])
-  K10revt= K10rev(X[i, t], Y[i, t], s[t] + delta[i])
-
-  K10t= K10(X[i, t], Y[i, t], s[t])
-  K01revt=K01rev(X[i, t], Y[i, t], s[t] + delta[i])
 
   #Clock
   c01 = [0.0 for i in range(N)]
@@ -125,27 +122,36 @@ for t in range(nsteps - 1):
   e01 = -np.log(np.random.random(size=N))   # realisation of an exponential of parameter 1
   e10 = -np.log(np.random.random(size=N))
 
-  #Clock
+  #Clock_rev
   c01rev = [0.0 for i in range(N)]
   c10rev = [0.0 for i in range(N)]
   e01rev = -np.log(np.random.random(size=N))   # realisation of an exponential of parameter 1
   e10rev = -np.log(np.random.random(size=N))
 
-  #s dynamics
-  s[t + 1] = s[t] + F*dt/v - (dt/v)*sum([alpha[i, t]*dxw1(s[t] + delta[i], Y[i, t]) for i in range(N)])
-  #torus condition on s
-  if s[t + 1] < -d/2 :
-    s[t + 1] += d
-  elif s[t + 1] > d/2 :
-    s[t + 1] -= d
-
+  
+  #precompute for s
+  delta_s = F*dt/v - (dt/v)*sum([alpha[i, t]*dxw1(s[i,t], Y[i, t]) for i in range(N)])
   for i in range(N) :
+    #s dynamics
+    s[i,t + 1] = s[i,t] + delta_s
+     #torus condition on s
+    if s[i,t + 1] < -d/2 :
+      s[i,t + 1] += d
+    elif s[i,t + 1] > d/2 :
+      s[i,t + 1] -= d
     #generating random numbers to decide if a jump takes place between t and t+dt
     x = random.random()
     y = random.random()
     #X and Y brownian motions
     Bx = random.gauss(0, 1)
     By = random.gauss(0, 1)
+
+      #Compute the rates K and K_rev
+    K01t= K01(X[i, t], Y[i, t], s[i,t])
+    K10revt= K10rev(X[i, t], Y[i, t], s[i,t])
+
+    K10t= K10(X[i, t], Y[i, t], s[i,t])
+    K01revt=K01rev(X[i, t], Y[i, t], s[i,t])
     
     
     
@@ -160,7 +166,7 @@ for t in range(nsteps - 1):
       #alpha and X dynamics
       if c01[i] > e01[i] or c10rev[i] > e10rev[i]:
         alpha[i, t + 1] = 1
-        X[i, t + 1] = s[t + 1] + delta[i]
+        X[i, t + 1] = s[i,t + 1]
         c01[i]=0.0
         e01[i]=-np.log(np.random.random())
         c10rev[i]=0.0
@@ -179,25 +185,37 @@ for t in range(nsteps - 1):
       c10[i]+= K10t*dt
 
       #alpha and X dynamics
-      prob = [(K10(j*dx, Y[i, t], s[t] + delta[i]) + K01rev(j*dx, Y[i, t], s[t] + delta[i]))*dx for j in range(npos)] #space discretized probabilities of detachment
-      detach_rate = sum(prob)  #calculate the overall detachment rate
+     #calculate the overall detachment rate
       if c01rev[i] > e01rev[i] or c10[i] > e10[i]:   # guard: no detachment possible, skip the jump
         alpha[i, t + 1] = 0
-        X[i, t + 1] = s[t] + delta[i] + np.random.choice(positions, p = (1/detach_rate)*np.array(prob))
+        X[i, t + 1] = s[i,t] 
         c01rev[i]=0.0
         e01rev[i]=-np.log(np.random.random())
         c10[i]=0.0
         e10[i]=-np.log(np.random.random())
       else :
         alpha[i, t + 1] = 1
-        X[i, t + 1] = s[t + 1] + delta[i]
+        X[i, t + 1] = s[i,t + 1]
 
 
 #Visualization of the results
-fig, axs = plt.subplots(nrows=1, ncols=2)
+fig, axs = plt.subplots(nrows=3, figsize=(18,18))
 
-axs[0].plot([dt*t for t in range(nsteps)], s)
-#axs[0].plot([dt*t for t in range(nsteps)], X[0, :])
-axs[1].plot([dt*t for t in range(nsteps)], Y[0, :])
+axs[0].plot([dt*t for t in range(nsteps)], X[0, :],label='X')
+axs[0].plot([dt*t for t in range(nsteps)], s[0,:],label='s')
+axs[0].set_xlabel("time")
+axs[0].set_ylabel("X_t")
+axs[0].legend()
+axs[0].set_title("X over time")
+
+axs[1].plot([dt*t for t in range(nsteps)], Y[0, :],label='Y')
+axs[1].set_xlabel("time")
+axs[1].set_ylabel("Y_t")
+axs[1].set_title("Y over time")
+
+axs[2].plot([dt*t for t in range(nsteps)], alpha[0, :],label='alpha')
+axs[2].set_xlabel("time")
+axs[2].set_ylabel(r"$\alpha$")
+axs[2].set_title(r"$\alpha$ over time")
 
 plt.show()
